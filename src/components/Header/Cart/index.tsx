b389851e5048b0e55b45268b1b28de868525db88
Jonathan Almeida <jcalmeida.jonathan@gmail.com>
2024-04-25 20:50:43 -0300

    context cart

--- src/components/Header/Cart/index.tsx
+++ src/components/Header/Cart/index.tsx
@@ -2,11 +2,12 @@
 import { cookies } from "next/headers";
 import Bag from "@/components/svg/bag.svg";
 import { Container, Quantity, Texts, Title } from "./styles";
+import { CartContext } from "@/context/cart";
+import { useContext } from "react";
 
 const Cart = () => {
-  const cookieCart = cookies().get("product")?.value;
-  const cart: productType[] = cookieCart ? JSON.parse(cookieCart) : null;
-  const totalItems = cart.reduce((acc, product) => {
+  const { addItemOnCart, cart } = useContext(CartContext);
+  const totalItems = cart.reduce((acc: any, product: any) => {
     const quantity = product.qtd || 1;
     return acc + product.price * quantity;
   }, 0);
