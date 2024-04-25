0c3c95848cfb994adcac3baa9fc8dc68d6328c78
Jonathan Almeida <jcalmeida.jonathan@gmail.com>
2024-04-25 18:50:53 -0300

    erro2

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -16,15 +16,8 @@ export default function Error({
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+      <h2>{error.message}</h2>
+
     </div>
   );
 }
